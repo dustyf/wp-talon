f3ca4e137e546cc94d411695c8cdcb4e6f8811b9
Dustin Filippini <dusty@dustyf.com>
2013-02-02 11:49:02 -0600

    Remove ping back and xfn from header

--- header.php
+++ header.php
@@ -17,8 +17,6 @@
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicon.ico" />
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->

b95739e64127dfd93464ab2d79f87040d6008a23
Dustin Filippini <dusty@dustyf.com>
2013-02-02 11:39:43 -0600

    adding favicon to header adding images folder

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicon.ico" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>

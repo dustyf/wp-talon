764cb406491c4f9f3c7f5d8b75dd160e30620ec5
Dustin Filippini <dusty@dustyf.com>
2013-10-14 12:49:20 -0500

    Moving scripts and styles out of functions.php and into an include

---
+++ inc/scripts-styles.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Enqueue scripts and styles
+ */
+function wp_talon_scripts() {
+	wp_enqueue_style( 'wp-talon-style', get_stylesheet_uri() );
+
+	wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '20130203', 'all' );
+	wp_enqueue_style( 'normalize' );
+
+	wp_register_style( 'foundation', get_template_directory_uri() . '/css/foundation.min.css', array(), '20131013', 'all' );
+	wp_enqueue_style( 'foundation' );
+
+	wp_enqueue_script( 'wp-talon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	if ( is_singular() && wp_attachment_is_image() ) {
+		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+	}
+
+	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr-2.6.2.min.js', array(), '20130202' );
+}
+add_action( 'wp_enqueue_scripts', 'wp_talon_scripts' );
